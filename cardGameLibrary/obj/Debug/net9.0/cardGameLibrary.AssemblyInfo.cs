7915483c2ad7c9c68d36c5c2a82c6335ceba8121
Globaltune <202308378@post.au.dk>
2025-09-01 17:46:12 +0200

    update and adds

--- cardGameLibrary/obj/Debug/net9.0/cardGameLibrary.AssemblyInfo.cs
+++ cardGameLibrary/obj/Debug/net9.0/cardGameLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cardGameLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53a4ffd6c7a2bac6eec37d4139eb96dec5293900")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+183b07c00b05c42161ddb51fc052daa935bf1305")]
 [assembly: System.Reflection.AssemblyProductAttribute("cardGameLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cardGameLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- cardGay/obj/Debug/net9.0/cardGay.AssemblyInfo.cs
+++ cardGay/obj/Debug/net9.0/cardGay.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cardGay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53a4ffd6c7a2bac6eec37d4139eb96dec5293900")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+183b07c00b05c42161ddb51fc052daa935bf1305")]
 [assembly: System.Reflection.AssemblyProductAttribute("cardGay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cardGay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
